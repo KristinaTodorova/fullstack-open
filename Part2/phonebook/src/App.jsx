bd846b1897796075489c6958f348f8b6b5212cc2
KristinaTodorova <krisitodorowa@gmail.com>
2025-01-07 11:22:47 +0200

    Exercise 2.7

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -11,8 +11,12 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
     const personObject = {
-    name: newName
+      name: newName
     }
     setPersons(persons.concat(personObject))
     setNewName('')
