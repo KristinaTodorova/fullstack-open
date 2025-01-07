8074ee045f09c9aca698f2b15e80e826106731e3
KristinaTodorova <krisitodorowa@gmail.com>
2025-01-08 01:42:20 +0200

    part 2c

--- Part2/part2/src/App.jsx
+++ Part2/part2/src/App.jsx
@@ -1,5 +1,19 @@
-const App = (props) => {
-  const { notes } = props
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   return (
     <div>
