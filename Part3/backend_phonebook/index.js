8438f30afbbb4f0441c3c493e2d81940e5b780d0
KristinaTodorova <krisitodorowa@gmail.com>
2025-02-02 20:19:51 +0200

    Exercise 3.5

--- Part3/backend_phonebook/index.js
+++ Part3/backend_phonebook/index.js
@@ -56,6 +56,38 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
+const generateId = () => {
+    const newId = persons.length > 0
+      ? Math.floor(Math.random() * 1500)
+      : 0
+    return String(newId)
+  }
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'Name is missing' 
+      })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'Name is missing' 
+        })
+      }
+  
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
