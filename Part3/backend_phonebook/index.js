f5f69f2c86c765f2d8f887a82681f77a04924a1e
KristinaTodorova <krisitodorowa@gmail.com>
2025-02-03 16:50:42 +0200

    added cors

--- Part3/backend_phonebook/index.js
+++ Part3/backend_phonebook/index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 const bodyparser = require('body-parser');
+const cors = require('cors');
+
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
 app.use(bodyparser.json());
+app.use(cors());
 
 let persons = [
     { 
@@ -110,7 +113,7 @@ app.post('/api/persons', (request, response) => {
   })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
