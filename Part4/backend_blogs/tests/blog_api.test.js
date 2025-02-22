353bdbe4d405850187e5abc5f062c66df1230b36
KristinaTodorova <krisitodorowa@gmail.com>
2025-02-22 14:17:26 +0200

    Exercise 4.9

--- Part4/backend_blogs/tests/blog_api.test.js
+++ Part4/backend_blogs/tests/blog_api.test.js
@@ -1,14 +1,14 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach} = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const Blog = require('../models/blog')
+//const Blog = require('../models/blog')
 
 const api = supertest(app)
 
 
-const initialBlogs = [
+/* const initialBlogs = [
     {
         title: "How to add async await to an app without f-in it up",
         author: "Me",
@@ -29,19 +29,30 @@ beforeEach(async () => {
     await blogObject.save()
     blogObject = new Blog(initialBlogs[1])
     await blogObject.save()
-})
+}) */
 
-test.only('blogs are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test.only('there are two blogs', async () => {
+test('there are nine blogs', async () => {
     const response = await api.get('/api/blogs')
-  
-    assert.strictEqual(response.body.length, 2)
+    assert.strictEqual(response.body.length, 9)
+  })
+
+after(async () => {
+  await mongoose.connection.close()
+})
+
+test('unique identifier is named id', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+        assert.strictEqual("id" in blog, true)
+        assert.strictEqual("_id" in blog, false)
+    })
   })
 
 after(async () => {
