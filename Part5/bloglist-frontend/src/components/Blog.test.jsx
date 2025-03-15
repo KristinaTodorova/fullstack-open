d30af84438e11c5614eb7028321a716174d70393
KristinaTodorova <krisitodorowa@gmail.com>
2025-03-15 16:15:43 +0200

    Exercise 5.14

--- Part5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -9,7 +10,7 @@ test('renders content', () => {
     likes: 10
   }
 
-  const { container } = render(<Blog blog={blog} />)
+  const { container } = render(<Blog blog={blog}/>)
 
   const div = container.querySelector('.blog')
   expect(div).toHaveTextContent(
@@ -24,4 +25,40 @@ test('renders content', () => {
   expect(div).not.toHaveTextContent(
     '10'
   )
+  
 })
+
+test('clicking the button shows blog details', async () => {
+    const user = {username: "kristinatodorova"}
+    const blog = {
+        title: 'Test with react-testing-library',
+        author: 'Kristina',
+        url: 'google.com',
+        likes: 10,
+        user: { username: 'kristinatodorova'}
+    }
+    
+    const mockHandler = vi.fn()
+  
+    const { container } = render(<Blog blog={blog} toggleVisibility = {mockHandler} user={user}/>)
+
+    const div = container.querySelector('.blog')
+
+    expect(div).not.toHaveTextContent(
+        'google.com'
+      )
+      expect(div).not.toHaveTextContent(
+        '10'
+      )
+  
+    const userAction = userEvent.setup()
+    const button = screen.getByText('View')
+    await userAction.click(button)
+  
+    expect(div).toHaveTextContent(
+        'google.com'
+      )
+      expect(div).toHaveTextContent(
+        '10'
+      )
+  })
