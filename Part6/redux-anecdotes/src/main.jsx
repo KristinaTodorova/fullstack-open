06fb1652111a8bf1a8acbc89f3aa824a6303c994
KristinaTodorova <krisitodorowa@gmail.com>
2025-03-24 00:05:22 +0200

    Exercise 6.12

--- Part6/redux-anecdotes/src/main.jsx
+++ Part6/redux-anecdotes/src/main.jsx
@@ -3,25 +3,6 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
 
-
-/* const combinedReducer = combineReducers({
-  anecdotes: reducer,
-  filter: filterReducer
-}) */
-
-/* const store = createStore(combinedReducer)
-store.subscribe(() => {
-  store.getState()
-})
- */
-
-/* const store = configureStore({
-  reducer: {
-    anecdotes: reducer,
-    filter: filterReducer
-  }
-}) */
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
---
+++ Part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = 'Notification test'
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    setNotification(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export const {setNotification} = notificationSlice.actions
+export default notificationSlice.reducer
